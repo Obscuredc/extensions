f7d922db03383046db19c1f349ec3dec403aec77
Obscuredc <77344064+Obscuredc@users.noreply.github.com>
2021-06-07 10:29:23 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,7 @@
+let divs = document.getElementsByClassName("ad-wrapper");
+for (i = 0; i < divs.length; i++) {
+    divs[i].innerHTML = "";
+}
+var colDiv = document.getElementsByClassName('section');
+colDiv[0].innerHTML = "";
+//obscuredc on github :)
