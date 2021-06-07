44882ed6032d63b787cf13f42e4fcbdc4c55a4e7
Obscuredc <77344064+Obscuredc@users.noreply.github.com>
2021-06-07 10:29:02 -0400

    Delete content.js

--- content.js
+++
@@ -1,6 +0,0 @@
-var d = document.getElementsByClassName('kix-appview-editor');
-d[0].style.backgroundImage = 'url(https://th.bing.com/th/id/R4393699599c97880c4163c8dc7d5de1d?rik=%2fqqdKwmnQaXlSg&pid=ImgRaw)';
-d[0].style.backgroundPosition = 'center';
-d[0].style.backgroundRepeat = 'no-repeat';
-d[0].style.backgroundSize = 'cover';
-//obscuredc on github :)
