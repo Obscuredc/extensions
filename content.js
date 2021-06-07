9c74b720a322bfd6d60baab39f0599da186cb4bf
Obscuredc <77344064+Obscuredc@users.noreply.github.com>
2021-06-07 10:28:03 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,6 @@
+var d = document.getElementsByClassName('kix-appview-editor');
+d[0].style.backgroundImage = 'url(https://th.bing.com/th/id/R4393699599c97880c4163c8dc7d5de1d?rik=%2fqqdKwmnQaXlSg&pid=ImgRaw)';
+d[0].style.backgroundPosition = 'center';
+d[0].style.backgroundRepeat = 'no-repeat';
+d[0].style.backgroundSize = 'cover';
+//obscuredc on github :)
